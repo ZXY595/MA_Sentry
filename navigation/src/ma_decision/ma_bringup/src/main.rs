4e196f7a47601dff9d0eba99f4ab943a09b79d98
ZXY595 <3303057136@qq.com>
2025-03-05 22:39:59 +0800

    [Add] Add cmd vel chassis speed reset script and test rustdds publish in ros2.

--- navigation/src/ma_decision/ma_bringup/src/main.rs
+++ navigation/src/ma_decision/ma_bringup/src/main.rs
@@ -1,18 +1,35 @@
-use ros2_client::{MessageTypeName, NodeName, ros2::QosPolicies};
+use ros2_client::{
+    MessageTypeName, Name, NodeName,
+    ros2::{self, QosPolicies, policy},
+};
 use ros2_interfaces_humble::geometry_msgs::msg::{Twist, Vector3};
 
 fn main() -> Result<(), anyhow::Error> {
     let ctx = ros2_client::Context::new()?;
     let mut node = ctx.new_node(NodeName::new("/ma", "bringup")?, Default::default())?;
-    let chassis_topic = ctx.create_topic(
-        "cmd_vel_chassis".into(),
+    let chassis_topic = node.create_topic(
+        &Name::new("/", "cmd_vel_chassis")?,
         MessageTypeName::new("geometry_msgs", "Twist"),
-        &QosPolicies::default(),
+        &QosPolicies::builder()
+            .history(policy::History::KeepLast { depth: 10 })
+            .reliability(policy::Reliability::Reliable {
+                max_blocking_time: ros2::Duration::from_millis(100),
+            })
+            .durability(policy::Durability::TransientLocal)
+            .build(),
     )?;
     let chassis_publisher = node.create_publisher::<Twist>(&chassis_topic, None)?;
-    chassis_publisher.publish(Twist {
-        linear: Vector3::default(),
-        angular: Vector3::default(),
-    })?;
-    Ok(())
+
+    smol::spawn(node.spinner()?.spin()).detach();
+
+    smol::block_on::<Result<(), anyhow::Error>>(async {
+        chassis_publisher.wait_for_subscription(&node).await;
+        chassis_publisher
+            .async_publish(Twist {
+                linear: Vector3::default(),
+                angular: Vector3::default(),
+            })
+            .await?;
+        Ok(())
+    })
 }
---
+++ navigation/stop_vel.sh
@@ -0,0 +1,1 @@
+ros2 topic pub -t 1 /cmd_vel_chassis geometry_msgs/Twist 
