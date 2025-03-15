bac21df55dfe5776e0410ac1b0f0ba238b957483
ZXY <3303057136@qq.com>
2025-03-15 13:03:58 +0800

    [Update] Replace  old map with new map.

--- navigation/run_real.sh
+++ navigation/run_real.sh
@@ -2,7 +2,7 @@ echo "123" | sudo -S chmod 666 /dev/ttyACM0 #(串口赋权)
 source ./install/setup.sh
 
 ros2 launch rm_nav_bringup bringup_real.launch.py \
-    world:=RMUC \
+    world:=RMUL \
     mode:=mapping  \
     localization:=slam_toolbox \
     lio:=pointlio \
--- navigation/run_simu.sh
+++ navigation/run_simu.sh
@@ -1,8 +1,8 @@
 source ./install/setup.sh
 
 ros2 launch rm_nav_bringup bringup_sim.launch.py \
-    world:=RMUC \
+    world:=RMUL \
     mode:=mapping \
     lio:=pointlio \
-    lio_rviz:=True \
+    lio_rviz:=False \
     nav_rviz:=True
