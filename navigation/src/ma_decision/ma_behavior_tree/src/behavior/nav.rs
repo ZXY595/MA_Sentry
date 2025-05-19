3e8b9105dcbafdc2d70d1d0b8c6830797655e727
ZXY <3303057136@qq.com>
2025-05-19 14:19:21 +0800

    [Feat] update bt

--- navigation/src/ma_decision/ma_behavior_tree/src/behavior/nav.rs
+++ navigation/src/ma_decision/ma_behavior_tree/src/behavior/nav.rs
@@ -3,16 +3,16 @@ use std::time::Duration;
 use anyhow::anyhow;
 use futures_util::TryFutureExt;
 use ma_interfaces::geometry_msgs::msg::Point;
-use smol::{future::FutureExt as SmolFutureExt, Timer};
+use smol::{Timer, future::FutureExt as SmolFutureExt};
+use thiserror::Error;
 
-use crate::sentry_action::nav_to_pose as nav_action;
+use crate::sentry_action::nav_to_pose::{
+    self as nav_action, CancelGoalError, RequestResultError, SendGoalError,
+};
 
-use super::{SentryCtx, Shutdown};
+use super::{SentryCtx, Shutdown, ShutdownReason};
 
-pub async fn patrol(
-    ctx: &SentryCtx,
-    shutdown: Shutdown,
-) -> Result<(), anyhow::Error> {
+pub async fn patrol(ctx: &SentryCtx, shutdown: Shutdown) -> Result<(), anyhow::Error> {
     loop {
         nav_to_point(
             ctx,
@@ -42,7 +42,7 @@ pub async fn patrol(
     }
 }
 
-/// when terminate signal is triggered, cancel the goal and return an error
+/// when shutdown signal is triggered, cancel the goal and return an error
 pub async fn nav_to_point(
     ctx: &SentryCtx,
     point: Point,
@@ -50,19 +50,24 @@ pub async fn nav_to_point(
 ) -> Result<(), anyhow::Error> {
     log::info!("Start nav to point: {point:?}");
     let client = &ctx.nav_clinet;
-    let (goal_id, stamp) = match nav_action::start_nav(client, point.clone()).await {
-        Err(e @ nav_action::SendGoalError::Timeout(_)) => {
-            log::warn!("Goal response timeout: {e}");
-            return Ok(());
+    let (goal_id, stamp) = loop {
+        match nav_action::start_nav(client, point.clone()).await {
+            Ok(result) => break result,
+            Err(e) => log::warn!("Fail to send goal: {e}"),
         }
-        other => other?,
     };
-    nav_action::request_result(client, goal_id)
-        .map_err(|e| anyhow!(e))
-        .or(async {
-            shutdown.wait_shutdown_triggered().await;
-            nav_action::cancel_goal(client, goal_id, stamp).await?;
-            Err(anyhow!("Nav canceled by terminate signal"))
-        })
-        .await
+    async {
+        if let Err(e) = nav_action::request_result(client, goal_id).await {
+            log::error!("Fail to request result: {e}")
+        }
+        Ok(())
+    }
+    .or(async {
+        let reason = shutdown.wait_shutdown_triggered().await;
+        nav_action::cancel_goal(client, goal_id, stamp).await?;
+        Err(anyhow!("Nav canceled with reason: {reason}"))
+    })
+    .await
 }
+
+
